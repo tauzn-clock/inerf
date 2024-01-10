69e2592e4d4cc0c8ecbd5764bb2ddf9f99695bbd
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2024-01-10 12:34:35 +0000

    bug fix

---
+++ inerf/inerf_dataparser.py
@@ -0,0 +1,7 @@
+from nerfstudio.data.dataparsers.nerfstudio_dataparser import NerfstudioDataParserConfig, Nerfstudio
+
+class INerfDataParserConfig(NerfstudioDataParserConfig):
+    pass
+
+class INerfDataParser(Nerfstudio):
+    pass
--- inerf/inerf_trainer.py
+++ inerf/inerf_trainer.py
@@ -1,4 +1,3 @@
-
 import dataclasses
 import functools
 import os
@@ -88,47 +87,8 @@ class INerfTrainer(Trainer):
                 'test': loads train/test datasets into memory
                 'inference': does not load any dataset into memory
         """
-        self.pipeline = self.config.pipeline.setup(
-            device=self.device,
-            test_mode=test_mode,
-            world_size=self.world_size,
-            local_rank=self.local_rank,
-            grad_scaler=self.grad_scaler,
-        )
-        print(123)
+        self.pipeline = self.config.pipeline
         self.optimizers = self.setup_optimizers()
-
-        # set up viewer if enabled
-        viewer_log_path = self.base_dir / self.config.viewer.relative_log_filename
-        self.viewer_state, banner_messages = None, None
-        if self.config.is_viewer_legacy_enabled() and self.local_rank == 0:
-            datapath = self.config.data
-            if datapath is None:
-                datapath = self.base_dir
-            self.viewer_state = ViewerLegacyState(
-                self.config.viewer,
-                log_filename=viewer_log_path,
-                datapath=datapath,
-                pipeline=self.pipeline,
-                trainer=self,
-                train_lock=self.train_lock,
-            )
-            banner_messages = [f"Legacy viewer at: {self.viewer_state.viewer_url}"]
-        if self.config.is_viewer_enabled() and self.local_rank == 0:
-            datapath = self.config.data
-            if datapath is None:
-                datapath = self.base_dir
-            self.viewer_state = ViewerState(
-                self.config.viewer,
-                log_filename=viewer_log_path,
-                datapath=datapath,
-                pipeline=self.pipeline,
-                trainer=self,
-                train_lock=self.train_lock,
-                share=self.config.viewer.make_share_url,
-            )
-            banner_messages = [f"Viewer at: {self.viewer_state.viewer_url}"]
-        self._check_viewer_warnings()
 
         self._load_checkpoint()
 
@@ -139,23 +99,7 @@ class INerfTrainer(Trainer):
                 pipeline=self.pipeline,
             )
         )
-
-        # set up writers/profilers if enabled
-        writer_log_path = self.base_dir / self.config.logging.relative_log_dir
-        writer.setup_event_writer(
-            self.config.is_wandb_enabled(),
-            self.config.is_tensorboard_enabled(),
-            self.config.is_comet_enabled(),
-            log_dir=writer_log_path,
-            experiment_name=self.config.experiment_name,
-            project_name=self.config.project_name,
-        )
-        writer.setup_local_writer(
-            self.config.logging, max_iter=self.config.max_num_iterations, banner_messages=banner_messages
-        )
-        writer.put_config(name="config", config_dict=dataclasses.asdict(self.config), step=0)
-        profiler.setup_profiler(self.config.logging, writer_log_path)
-
+        
     def setup_optimizers(self) -> Optimizers:
         """Helper to set up the optimizers
 
@@ -170,88 +114,29 @@ class INerfTrainer(Trainer):
         """Train the model."""
         assert self.pipeline.datamanager.train_dataset is not None, "Missing DatsetInputs"
 
-        self.pipeline.datamanager.train_dataparser_outputs.save_dataparser_transform(
-            self.base_dir / "dataparser_transforms.json"
-        )
+        print(123)
 
-        self._init_viewer_state()
-        with TimeWriter(writer, EventName.TOTAL_TRAIN_TIME):
-            num_iterations = self.config.max_num_iterations
-            step = 0
-            for step in range(self._start_step, self._start_step + num_iterations):
-                while self.training_state == "paused":
-                    time.sleep(0.01)
-                with self.train_lock:
-                    with TimeWriter(writer, EventName.ITER_TRAIN_TIME, step=step) as train_t:
-                        self.pipeline.train()
+        for step in range(3):
+            while self.training_state == "paused":
+                time.sleep(0.01)
+            with self.train_lock:
+                self.pipeline.train()
 
-                        # training callbacks before the training iteration
-                        for callback in self.callbacks:
-                            callback.run_callback_at_location(
-                                step, location=TrainingCallbackLocation.BEFORE_TRAIN_ITERATION
-                            )
-
-                        # time the forward pass
-                        loss, loss_dict, metrics_dict = self.train_iteration(step)
-
-                        # training callbacks after the training iteration
-                        for callback in self.callbacks:
-                            callback.run_callback_at_location(
-                                step, location=TrainingCallbackLocation.AFTER_TRAIN_ITERATION
-                            )
-
-                # Skip the first two steps to avoid skewed timings that break the viewer rendering speed estimate.
-                if step > 1:
-                    writer.put_time(
-                        name=EventName.TRAIN_RAYS_PER_SEC,
-                        duration=self.world_size
-                        * self.pipeline.datamanager.get_train_rays_per_batch()
-                        / max(0.001, train_t.duration),
-                        step=step,
-                        avg_over_steps=True,
+                # training callbacks before the training iteration
+                for callback in self.callbacks:
+                    callback.run_callback_at_location(
+                        step, location=TrainingCallbackLocation.BEFORE_TRAIN_ITERATION
                     )
 
-                self._update_viewer_state(step)
+                # time the forward pass
+                loss, loss_dict, metrics_dict = self.train_iteration(step)
 
-                # a batch of train rays
-                if step_check(step, self.config.logging.steps_per_log, run_at_zero=True):
-                    writer.put_scalar(name="Train Loss", scalar=loss, step=step)
-                    writer.put_dict(name="Train Loss Dict", scalar_dict=loss_dict, step=step)
-                    writer.put_dict(name="Train Metrics Dict", scalar_dict=metrics_dict, step=step)
-                    # The actual memory allocated by Pytorch. This is likely less than the amount
-                    # shown in nvidia-smi since some unused memory can be held by the caching
-                    # allocator and some context needs to be created on GPU. See Memory management
-                    # (https://pytorch.org/docs/stable/notes/cuda.html#cuda-memory-management)
-                    # for more details about GPU memory management.
-                    writer.put_scalar(
-                        name="GPU Memory (MB)", scalar=torch.cuda.max_memory_allocated() / (1024**2), step=step
+                # training callbacks after the training iteration
+                for callback in self.callbacks:
+                    callback.run_callback_at_location(
+                        step, location=TrainingCallbackLocation.AFTER_TRAIN_ITERATION
                     )
-
-                # Do not perform evaluation if there are no validation images
-                if self.pipeline.datamanager.eval_dataset:
-                    self.eval_iteration(step)
-
-                if step_check(step, self.config.steps_per_save):
-                    self.save_checkpoint(step)
-
-                writer.write_out_storage()
-
-        # save checkpoint at the end of training
-        self.save_checkpoint(step)
-
-        # write out any remaining events (e.g., total train time)
-        writer.write_out_storage()
-
-        table = Table(
-            title=None,
-            show_header=False,
-            box=box.MINIMAL,
-            title_style=style.Style(bold=True),
-        )
-        table.add_row("Config File", str(self.config.get_base_dir() / "config.yml"))
-        table.add_row("Checkpoint Directory", str(self.checkpoint_dir))
-        CONSOLE.print(Panel(table, title="[bold][green]:tada: Training Finished :tada:[/bold]", expand=False))
-
+                        
         # after train end callbacks
         for callback in self.callbacks:
             callback.run_callback_at_location(step=step, location=TrainingCallbackLocation.AFTER_TRAIN)
@@ -404,11 +289,12 @@ class INerfTrainer(Trainer):
         Args:
             step: Current training step.
         """
-
         needs_zero = [
             group for group in self.optimizers.parameters.keys() if step % self.gradient_accumulation_steps[group] == 0
         ]
         self.optimizers.zero_grad_some(needs_zero)
+        print(needs_zero)
+        
         cpu_or_cuda_str: str = self.device.split(":")[0]
         cpu_or_cuda_str = "cpu" if cpu_or_cuda_str == "mps" else cpu_or_cuda_str
 
